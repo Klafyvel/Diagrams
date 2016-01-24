abb744a67c123eec1a870cdcbf3e02d14f55f49f
Klafyvel <klafyvel@gmail.com>
2016-01-24 14:33:27 +0100

    Ajout de la classe Canvas et de sa structure.

---
+++ data.py
@@ -0,0 +1,14 @@
+class Canvas:
+    def __init__(self):
+        pass
+    def add_participant(self, name):
+        pass
+    def add_note(self, **kwargs):
+        pass
+    def set_title(self, title):
+        pass
+    def add_link(self, **kwargs):
+        pass
+    def get_svg(self):
+        pass
+
